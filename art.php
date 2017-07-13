757c307dff7c38022db822b6b5c7c07c772851fb
Yang Liu <yang.liu@mdpi.com>
2017-07-13 13:29:52 +0800

    dix

--- art.php
+++ art.php
@@ -10,13 +10,13 @@ $art_id = $_GET['art_id'] + 0;
 
 //判断地址栏传来的art_id 是否合法
 /*if(!is_numeric($art_id)) {
-	header('Location: index.php');
+    header('Location: index.php');
 }*/
 
 //如果没有这篇文章 跳转到首页去
 /*$sql = "select * from art where art_id=$art_id";
 if(!mGetRow($sql)) {
-	header('Location: index.php');
+    header('Location: index.php');
 }*/
 
 //查询文章
@@ -32,21 +32,21 @@ $comms = mGetAll($sql);
 
 //post 非空 代表有留言
 if(!empty($_POST)) {
-	$comm['name'] = trim($_COOKIE['name']);
-	$comm['content'] = htmlspecialchars(trim($_POST['content']));
-	$comm['pubtime'] = date('Y-m-d h:i:s',time());
-	$comm['art_id'] = $art_id;
-	$comm['ip'] = sprintf('%u' , ip2long(getRealIp()));
-	$rs = mExec('comment' , $comm);
-	if($rs) {
-		//评论发布成功 将art表的comm+1
-		$sql = "update art set comm=comm+1 where art_id=$art_id";
-		mQuery($sql);
+    $comm['name'] = trim($_COOKIE['name']);
+    $comm['content'] = htmlspecialchars(trim($_POST['content']));
+    $comm['pubtime'] = date('Y-m-d h:i:s',time());
+    $comm['art_id'] = $art_id;
+    $comm['ip'] = sprintf('%u' , ip2long(getRealIp()));
+    $rs = mExec('comment' , $comm);
+    if($rs) {
+        //评论发布成功 将art表的comm+1
+        $sql = "update art set comm=comm+1 where art_id=$art_id";
+        mQuery($sql);
 
-		//跳转到上个页面
-		$ref = $_SERVER['HTTP_REFERER'];
-		header("Location: $ref");
-	}
+        //跳转到上个页面
+        $ref = $_SERVER['HTTP_REFERER'];
+        header("Location: $ref");
+    }
 }
 
 
